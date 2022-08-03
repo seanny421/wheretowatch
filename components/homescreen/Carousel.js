c9959564024ae3aebd837b0ce3ee3ddbd87470da
Sean McGeachie <sean@seans-air.home>
2022-08-03 23:20:18 +0100

    functional carousel that allows search panel dismissal

--- components/homescreen/Carousel.js
+++ components/homescreen/Carousel.js
@@ -1,39 +1,54 @@
 import { Text, ScrollView, StyleSheet, View, Image, Dimensions} from "react-native";
 import {useState, useEffect} from "react";
+import Slideshow from 'react-native-slideshow-improved';
 const {width} = Dimensions.get('window');
 const height = width * 100 / 90;
 
 export default function Carousel(){
     //state & variables
+    const [position, setPosition] = useState(0);
     const imgData = [
-        'https://cdn.dribbble.com/users/9378043/screenshots/16832559/netflix__1__4x.png',
-        'https://cdn.pastemagazine.com/www/articles/2020/01/02/prime-video-logo.png',
-        'https://www.shortlist.com/media/imager/202002/45711-product_widget.large.png'
+        {
+            url: require('../../assets/home_backgrounds/netflix-n-logo-png-itv-america-82897.png')
+        },
+        {
+            url: 'https://cdn.pastemagazine.com/www/articles/2020/01/02/prime-video-logo.png'
+        },
+        {
+            url: 'https://www.shortlist.com/media/imager/202002/45711-product_widget.large.png'
+        },
     ];
-    return(
-            <ScrollView pagingEnabled horizontal style={{width, height}}>
-                {
-                    imgData.map((image, index) => {
-                        return(
-                            <Image
-                                key={index}
-                                source={{uri: image}}
-                                style={styles.backgroundImage}
-                            />
-                        );
-                    })
-                }
+    //run once on component mount
+    useEffect(()=>{
+        //move position of carousel images by 1 every 5 seconds
+        //if we are on last image go back to first
+        setTimeout(() =>{
+            setPosition(position >= imgData.length ? 0 : position + 1);
+        }, 5000);
+    }, [position]);
 
-            </ScrollView>
+    return(
+        <View pointerEvents="none" style={styles.backgroundImage}>
+            {/* https://github.com/R3D347HR4Y/react-native-slideshow */}
+            {/* image source works with png */}
+            <Slideshow
+                dataSource={imgData}
+                position={position}
+                scrollEnabled={false}
+                indicatorSize={0}
+                arrowSize={0}
+                // onPositionChanged={position => setPosition({ position })}
+            />
+        </View>
     );
 }
 
 const styles = StyleSheet.create({
     backgroundImage: {
-        // width: '100%',
         width: width,
         height: height,
-        // height: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
     }
 
 });
